0726b6f4f25a45aee8c4ae0de9fbebce874bac3c
Philip-Machar <philipmachar788@gmail.com>
2024-08-11 07:07:37 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(8000, () => console.log(`server running at port 8000...`));
