bab429fffd1613ab43b1dc997ef01192a7f39b60
Philip-Machar <philipmachar788@gmail.com>
2024-08-11 08:30:11 +0300

    serving json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 //set up static folder
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 /*app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -14,4 +14,16 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'))
 });
 */
-app.listen(8000, () => console.log(`server running at port 8000...`));
+
+let posts = [
+    {id: 1, title: 'one'},
+    {id: 2, title: 'two'},
+    {id: 3, title: 'three'}
+];
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+});
+
+port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`server running at port ${port}...`));
