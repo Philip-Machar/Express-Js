06a11d5d3e2e3a7bbc757cb9360b31a3fb2d03ca
Philip-Machar <philipmachar788@gmail.com>
2024-08-11 07:39:53 +0300

    set up static folder

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+//set up static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'))
+});
+*/
 app.listen(8000, () => console.log(`server running at port 8000...`));
