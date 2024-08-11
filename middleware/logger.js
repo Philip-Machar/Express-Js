82bf44536280a2de9f4805fcde197658cebcdd50
Philip-Machar <philipmachar788@gmail.com>
2024-08-11 22:05:43 +0300

    Logger middleware

---
+++ middleware/logger.js
@@ -0,0 +1,7 @@
+//Logger middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+export default logger;
