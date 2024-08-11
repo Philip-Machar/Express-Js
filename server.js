c2968c5d99accdfeb1e2fef42053012a025a86a2
Philip-Machar <philipmachar788@gmail.com>
2024-08-11 22:04:56 +0300

    Global middleware

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
 
 const app = express();
 
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//Logger middleware
+app.use(logger);
 
 //set up static folder
 //app.use(express.static(path.join(__dirname, 'public')));
