22c6677b504fa850a1843ecc04cd2fd628034a3f
Philip-Machar <philipmachar788@gmail.com>
2024-08-12 15:26:42 +0300

    Complete CRUD api

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url'
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import error from './middleware/error.js'
 import notFound from './middleware/notFound.js'
 
 const app = express();
+
+//Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Body parser middleware
 app.use(express.json());
@@ -15,7 +20,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(logger);
 
 //set up static folder
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/api/posts', posts);
