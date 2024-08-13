42887755ac15c0a31d15575fa59a604b2b283743
Philip-Machar <philipmachar788@gmail.com>
2024-08-13 10:53:38 +0300

    Complete CRUD api

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+const getPostsBtn = document.querySelector('#get-posts-btn');
+const output = document.querySelector('#output'); 
+
+async function showPosts() {
+    try {
+        const res = await fetch('http://localhost:8080/api/posts/');
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch posts');
+        }
+
+        const posts = await res.json();
+        output.innerHTML = '';
+
+        posts.forEach((post) => {
+            const postElement = document.createElement('div');
+            postElement.textContent = post.title;
+            output.appendChild(postElement);
+        });
+    } catch (error) {
+        console.log('Error fetching posts: ', error);
+    }
+}
+
+// Event listener
+getPostsBtn.addEventListener('click', showPosts); // Removed the parentheses to pass function reference
