f5ac1a67835b47e75ae23a2bfb1354ac6c49c831
Philip-Machar <philipmachar788@gmail.com>
2024-08-14 22:00:03 +0300

    Complete CRUD api

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const getPostsBtn = document.querySelector('#get-posts-btn');
 const output = document.querySelector('#output'); 
+const form = document.querySelector('#add-post-form'); 
 
 async function showPosts() {
     try {
@@ -22,5 +23,38 @@ async function showPosts() {
     }
 }
 
-// Event listener
+//Submit new post
+async function addPost(e){
+    e.preventDefault();
+
+    const formData = new FormData(this);
+    const title = formData.get('title');
+
+    try {
+        const res = await fetch('http://localhost:8080/api/posts/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title})
+        });
+
+        if (!res.ok){
+            throw new Error('Failed to add new post');
+        }
+
+        const newPost = await res.json();
+
+        const postEl = document.createElement('div');
+        postEl.textContent = newPost.title;
+
+        output.appendChild(postEl);
+        showPosts();
+    } catch (error) {
+        console.error('Error adding post');
+    }
+}
+
+// Event listeners
 getPostsBtn.addEventListener('click', showPosts); 
+form.addEventListener('submit', addPost);
